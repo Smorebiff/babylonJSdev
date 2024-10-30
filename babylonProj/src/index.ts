533b33e34d03f59961c3dad947d828c1924196fe
Zane <156668313+Smorebiff@users.noreply.github.com>
2024-10-30 12:46:55 +0000

    works for real

---
+++ babylonProj/src/index.ts
@@ -0,0 +1,17 @@
+import { Engine } from "@babylonjs/core";
+import createStartScene from "/workspaces/babylonJSdev/babylonProj/src/createStartScreen";
+import './main.css';
+
+const CanvasName = "renderCanvas";
+
+let canvas = document.createElement("canvas");
+canvas.id = CanvasName;
+
+canvas.classList.add("background-canvas");
+document.body.appendChild(canvas);
+
+let eng = new Engine(canvas, true, {}, true);
+let startScene = createStartScene(eng);
+eng.runRenderLoop(() => {
+    startScene.scene.render();
+});                  
